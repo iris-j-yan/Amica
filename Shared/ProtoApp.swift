84e6bd9c6cafb22d5c19a9f52f396d9c46d32a4c
Iris <eyeris92@gmail.com>
2023-08-03 12:06:01 -0700

    Initial Commit

---
+++ Shared/ProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoApp.swift
+//  Shared
+//
+//  Created by Iris Yan on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
